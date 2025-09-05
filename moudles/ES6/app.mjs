4786cdd8c5cd1f1a28608d25ce5254c0c970a768
Arhum19 <arhumnoor10@gmail.com>
2025-09-05 18:27:20 +0500

    4th commit

---
+++ moudles/ES6/app.mjs
@@ -0,0 +1,5 @@
+import math from "./math.mjs";
+
+console.log(`My name is ${math.name} and i am ${math.age} years old.`
+  + ` These are the two function i've made`+
+   ` For addition: ${math.add(5,6)} and For subtraction: ${math.sub(5,2)}`);
