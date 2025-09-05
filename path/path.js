8042b0ba753e747349f8ff609ffe7c656dcf59a2
Arhum19 <arhumnoor10@gmail.com>
2025-09-05 19:29:24 +0500

    4th commit

---
+++ path/path.js
@@ -0,0 +1,8 @@
+let path =  require("path")
+
+console.log(path.basename("/user/filename/test.txt"));
+console.log(path.dirname("/user/filename/test.txt"));
+console.log(path.extname("/user/filename/test.txt"));
+console.log(path.join("/user","filename","test.txt"));
+console.log(__filename);
+console.log(__dirname);
