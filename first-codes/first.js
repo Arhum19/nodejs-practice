360119ae517a4bc777801ba40e4e40e212431862
Arhum19 <arhumnoor10@gmail.com>
2025-09-05 17:03:40 +0500

    2nd commit

--- first-codes/first.js
+++ first-codes/first.js
@@ -1,1 +1,6 @@
 console.log("Hello my fisrt node js code!!")
+let hours = new Date().getHours
+let name  = "arhum";
+let greting = (hours < 12 ? "Good Morning" : hours < 16 ? "Good Afternoon" : "Good Evening");
+console.log(`Hi ${name}, ${greting} this is our first node js code!`);
+
