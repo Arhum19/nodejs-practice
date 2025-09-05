a5857f2c05dab2523ce3eede29496a6cf36e7d32
Arhum19 <arhumnoor10@gmail.com>
2025-09-05 20:23:23 +0500

    5th commit

---
+++ FS/fs-async.js
@@ -0,0 +1,33 @@
+let fs = require("fs").promises;
+const { log } = require("console");
+let path = require("path");
+
+let folder = path.join(__dirname, "prod");
+let file = path.join(folder, "produciton.pdf");
+
+
+//first create a folder and check exitsing folder!
+async function createfolder() {
+try{
+    await fs.mkdir(folder, {recursive: true})
+    console.log("File has been created");
+    
+}
+catch(er){
+     console.log("There's an error in it",er);
+
+    }
+}
+createfolder();
+
+async function writef() {
+try{
+    await fs.writeFile(file, "This is an first text")
+    
+}
+catch(er){
+     console.log("There's an error in it",er);
+
+    }
+}
+writef();
