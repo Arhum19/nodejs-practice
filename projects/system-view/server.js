d1b72a4b6c6efbaacfadf9610b9d8c0f0e87a418
Arhum19 <arhumnoor10@gmail.com>
2025-09-07 20:11:02 +0500

    9th commit

--- projects/system-view/server.js
+++ projects/system-view/server.js
@@ -24,81 +24,103 @@ function formatUptime(seconds) {
   return `${days} days, ${hours} hours, ${minutes} minutes, ${seconds} seconds`;
 }
 
-
 // get cpu info
 let getcpu = () => {
-  let model =  os.cpus()[0].model 
-  let core =  os.cpus().length
+  let model = os.cpus()[0].model;
+  let core = os.cpus().length;
   let architecture = os.arch();
   let load_Average = os.loadavg();
 
   console.log({
-    model,core,architecture,load_Average
+    model,
+    core,
+    architecture,
+    load_Average,
   });
-}
+};
 
 //getcpu();
 
 // memort info
 let getmem = () => {
-  let Total_Memory =  os.totalmem(); 
-  let Remaining_Memory =  os.freemem();
+  let Total_Memory = os.totalmem();
+  let Remaining_Memory = os.freemem();
   let tot = Total_Memory - Remaining_Memory;
-  let usage = ((tot/Total_Memory*100).toFixed(decimal)+"%") 
+  let usage = ((tot / Total_Memory) * 100).toFixed(decimal) + "%";
 
   console.log({
     Total_Memory: formatBytes(Total_Memory, decimal),
     Remaining_Memory: formatBytes(Remaining_Memory, decimal),
-    Usage: usage
+    Usage: usage,
   });
-}
+};
 //getmem();
 
 // os info
 let getos = () => {
-  let platform =  os.platform(); 
-  let host =  os.hostname();
+  let platform = os.platform();
+  let host = os.hostname();
   let type = os.type();
-  let release  = os.release();
+  let release = os.release();
   let up = os.uptime();
 
   console.log({
-    platform,host,type,release,Up_Time:formatUptime(up)
+    platform,
+    host,
+    type,
+    release,
+    Up_Time: formatUptime(up),
   });
-}
+};
 //getos();
 
-let uinfo = () =>{
-    let user = os.userInfo()
-    console.log(user);
-    
-}
+let uinfo = () => {
+  let user = os.userInfo();
+  console.log(user);
+};
 //uinfo();
 
-let network = () =>{
-    let net = os.networkInterfaces()
-    console.log(net);
-    
-}
+let network = () => {
+  let net = os.networkInterfaces();
+  console.log(net);
+};
 // network();
 
-let processinfo = () =>{
-    let pid = process.pid;
-    let title = process.title;
-    let nodeversion = process.version;
-    let platform = process.platform;
-    let cwd = process.cwd();
-    let memoryUsage ={
-        rss:  formatBytes(process.memoryUsage().rss, decimal),
-        heapTotal: formatBytes(process.memoryUsage().heapTotal, decimal),
-        heapUsed: formatBytes(process.memoryUsage().heapUsed, decimal),
-        external: formatBytes(process.memoryUsage().external, decimal),  
-    }
-    
-    console.log({
-        pid,title,nodeversion,platform,cwd,memoryUsage
-    });
-}
+let processinfo = () => {
+  let pid = process.pid;
+  let title = process.title;
+  let nodeversion = process.version;
+  let platform = process.platform;
+  let cwd = process.cwd();
+  let memoryUsage = {
+    rss: formatBytes(process.memoryUsage().rss, decimal),
+    heapTotal: formatBytes(process.memoryUsage().heapTotal, decimal),
+    heapUsed: formatBytes(process.memoryUsage().heapUsed, decimal),
+    external: formatBytes(process.memoryUsage().external, decimal),
+  };
+  Node_Env: process.env.NODE_ENV || "server not set";
+
+  console.log({
+    pid,
+    title,
+    nodeversion,
+    platform,
+    cwd,
+    memoryUsage,
+  });
+};
 processinfo();
 
 //creating the http server for the application
+let r = {
+  "/": {
+    name: "Simple Json server!!",
+    description: "Acess system information via /system",
+    routes: ["/memory", "/cpu", "/os", "/user", "/process", "/network"],
+  },
+};
+
+const server = http.createServer( (req,res) =>{
+  res.setHeader("Content-Type", "json/application");
+  
+})
